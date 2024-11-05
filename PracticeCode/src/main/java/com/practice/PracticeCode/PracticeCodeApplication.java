a5c04e63804fca448551e297572bb0db67e4cca3
Rahul <rahulsinghdeoatwork@gmail.com>
2024-11-05 19:13:02 +0530

    intial commit

---
+++ PracticeCode/src/main/java/com/practice/PracticeCode/PracticeCodeApplication.java
@@ -0,0 +1,15 @@
+package com.practice.PracticeCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeCodeApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PracticeCodeApplication.class, args);
+		System.out.println("Welcome to");
+	}
+
+}
